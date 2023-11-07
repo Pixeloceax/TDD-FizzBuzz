5085bab0038b4b6a6faef2451a81b10ea4d4646f
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-07 21:38:30 +0100

    Duplicate all test cases for thorough validation

--- typescript/src/test/FizzBuzz.test.ts
+++ typescript/src/test/FizzBuzz.test.ts
@@ -6,10 +6,20 @@ describe("FizzBuzz", () => {
     FizzBuzz(3);
     expect(logSpy).toHaveBeenCalledWith("Fizz");
   });
+  it('should return "Fizz" for multiples of 3', () => {
+    const logSpy = jest.spyOn(console, "log");
+    FizzBuzz(6);
+    expect(logSpy).toHaveBeenCalledWith("Fizz");
+  });
 
   it('should return "Buzz" for multiples of 5', () => {
     const logSpy = jest.spyOn(console, "log");
     FizzBuzz(5);
+    expect(logSpy).toHaveBeenCalledWith("Buzz");
+  });
+  it('should return "Buzz" for multiples of 5', () => {
+    const logSpy = jest.spyOn(console, "log");
+    FizzBuzz(10);
     expect(logSpy).toHaveBeenCalledWith("Buzz");
   });
 
@@ -18,10 +28,20 @@ describe("FizzBuzz", () => {
     FizzBuzz(15);
     expect(logSpy).toHaveBeenCalledWith("FizzBuzz");
   });
+  it('should return "FizzBuzz" for multiples of 3 and 5', () => {
+    const logSpy = jest.spyOn(console, "log");
+    FizzBuzz(30);
+    expect(logSpy).toHaveBeenCalledWith("FizzBuzz");
+  });
 
   it("should return the number for non-multiples of 3 or 5", () => {
     const logSpy = jest.spyOn(console, "log");
     FizzBuzz(92);
     expect(logSpy).toHaveBeenCalledWith(92);
+  });
+  it("should return the number for non-multiples of 3 or 5", () => {
+    const logSpy = jest.spyOn(console, "log");
+    FizzBuzz(97);
+    expect(logSpy).toHaveBeenCalledWith(97);
   });
 });
