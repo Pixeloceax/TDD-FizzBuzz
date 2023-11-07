86d09773ac64851422875aa895b2ebcc3f5c7d25
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-07 20:53:33 +0100

    Add test for multiples of 3 in FizzBuzz function

---
+++ typescript/src/test/FizzBuzz.test.ts
@@ -0,0 +1,20 @@
+import { FizzBuzz } from "../FizzBuzz";
+
+describe("FizzBuzz", () => {
+  it('should return "Fizz" for multiples of 3', () => {
+    const expectFizzWhenModuloOF3 = FizzBuzz(3);
+    expect(expectFizzWhenModuloOF3).toBe("Fizz");
+  });
+
+  it('should return "Buzz" for multiples of 5', () => {
+    // TODO: Write test case
+  });
+
+  it('should return "FizzBuzz" for multiples of 3 and 5', () => {
+    // TODO: Write test case
+  });
+
+  it("should return the number for non-multiples of 3 or 5", () => {
+    // TODO: Write test case
+  });
+});
