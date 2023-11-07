ba6eb9b5995537780be0b5c64d65209e369d3724
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-07 21:13:55 +0100

    Implement FizzBuzz function to pass the test for multiples of 5

--- typescript/src/FizzBuzz.ts
+++ typescript/src/FizzBuzz.ts
@@ -1,5 +1,8 @@
 export const FizzBuzz = (input: number) => {
   if (input % 3 === 0) {
     console.log("Fizz");
-  } else console.log(input);
+  }
+  if (input % 5 === 0) {
+    console.log("Buzz");
+  }
 };
