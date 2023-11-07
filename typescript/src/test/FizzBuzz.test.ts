4885ca44477bae0844e2b8205fc65bb5d9294ebb
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-07 21:09:20 +0100

    Add test for multiples of 3 in FizzBuzz function

--- typescript/src/test/FizzBuzz.test.ts
+++ typescript/src/test/FizzBuzz.test.ts
@@ -2,8 +2,9 @@ import { FizzBuzz } from "../FizzBuzz";
 
 describe("FizzBuzz", () => {
   it('should return "Fizz" for multiples of 3', () => {
-    const resultForMultipleOf3 = FizzBuzz(3);
-    expect(resultForMultipleOf3).toBe("Fizz");
+    const logSpy = jest.spyOn(console, "log");
+    FizzBuzz(3);
+    expect(logSpy).toHaveBeenCalledWith("Fizz");
   });
 
   it('should return "Buzz" for multiples of 5', () => {
