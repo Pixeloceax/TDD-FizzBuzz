469ca4e496f2a4c7045793b87a0f98db4a1b8716
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-14 23:29:46 +0100

    TDD

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -1,3 +1,9 @@
-# FizzBuzz.py
-def FizzBuzz(number):
-   return number
+def FizzBuzz(number: int):
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
--- Python/main.py
+++ Python/main.py
@@ -2,9 +2,11 @@
 
 import FizzBuzz
 
+
 def main():
     for number in range(1, 101):
         print(FizzBuzz.FizzBuzz(number))
+
 
 if __name__ == '__main__':
     main()
